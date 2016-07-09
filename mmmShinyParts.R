2c4041f79a1e1e0ef1a140e360fcf09e023ef133
mikebesso <mike.besso@gmail.com>
2016-07-09 11:05:47 -0500

    shell files

--- mmmShinyParts.R
+++ mmmShinyParts.R
@@ -28,6 +28,9 @@ Icon <- list(
     plus = icon("plus"),
     minus = icon("minus"),
     times = icon("times")
+  ),
+  ui = list(
+    help = icon("question")
   )
 );
 
@@ -47,6 +50,57 @@ Panel <-
 
 
 
+mmmBox <- function (..., title = NULL, footer = NULL, status = NULL, solidHeader = FALSE,
+                    background = NULL, width = 6, height = NULL, collapsible = FALSE,
+                    collapsed = FALSE)
+{
+  boxClass <- "box"
+  if (solidHeader || !is.null(background)) {
+    boxClass <- paste(boxClass, "box-solid")
+  }
+  if (!is.null(status)) {
+    validateStatus(status)
+    boxClass <- paste0(boxClass, " box-", status)
+  }
+  if (collapsible && collapsed) {
+    boxClass <- paste(boxClass, "collapsed-box")
+  }
+  if (!is.null(background)) {
+    validateColor(background)
+    boxClass <- paste0(boxClass, " bg-", background)
+  }
+  style <- NULL
+  if (!is.null(height)) {
+    style <- paste0("height: ", validateCssUnit(height))
+  }
+  titleTag <- NULL
+  if (!is.null(title)) {
+    titleTag <- h3(class = "box-title", title)
+  }
+
+  helpTag <- div(class = "box-tools pull-right", a(href = "http://google.com", Icon$ui$help))
+
+  collapseTag <- NULL
+  if (collapsible) {
+    buttonStatus <- status %OR% "default"
+    collapseIcon <- if (collapsed)
+      "plus"
+    else "minus"
+    collapseTag <- div(class = "box-tools pull-right", tags$button(class = paste0("btn btn-box-tool"),
+                                                                   `data-widget` = "collapse", shiny::icon(collapseIcon)))
+  }
+  headerTag <- NULL
+  if (!is.null(titleTag) || !is.null(collapseTag)) {
+    headerTag <- div(class = "box-header", helpTag, titleTag, collapseTag)
+  }
+  div(class = if (!is.null(width))
+    paste0("col-sm-", width), div(class = boxClass, style = if (!is.null(style))
+      style, headerTag, div(class = "box-body", ...), if (!is.null(footer))
+        div(class = "box-footer", footer)))
+}
+
+
+
 Box <-
   list(
     Input = function(
@@ -61,7 +115,7 @@ Box <-
       collapsible = FALSE,
       collapsed = FALSE
     ){
-      box(
+      mmmBox(
         div(class = "shiny-input-panel", flowLayout(...)),
         title = title,
         footer = footer,
